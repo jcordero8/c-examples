b400008aeae16f31b591ff004bf76c50835a4da7
jcordero8 <joshua.corderocastillo@ucr.ac.cr>
2024-04-26 12:52:17 -0600

    cambios en main

--- funciones/9-encabezados.c
+++ funciones/9-encabezados.c
@@ -5,10 +5,11 @@ int main() {
 
     int v;
 
-    // Llama a las 3 funciones
+    // Llama a las 4 funciones
     funcion1();
     funcion2(5, 2.67);
     v = funcion3(10);
+    b = funcion4(10);
 
     // Imprime el valor que retornó funcion3()
     printf("v = %d\n", v);
@@ -35,3 +36,11 @@ int funcion3(int x) {
     int y = x + 2;
     return y;
 }
+// funcion4(): Imprime un mensaje. Retorna el valor que recibió como argumento
+// incrementado en 2
+int funcion4(int x) {
+    printf("En funcion4()...\n");
+    int y = x - 2;
+    return y;
+}
+~
